f40e6dee9d20f499663cc7fc672853b1459b17bb
Martin Renteria <renteriamp96@gmail.com>
2021-09-27 09:00:53 -0700

    Pushing current steps for task 7

--- js/index.js
+++ js/index.js
@@ -1,40 +1,79 @@
 let tasks = new TaskManager();
 
-// let taskHTML = createTaskHtml("hi","hi","hi","hi","hi")
-// console.log(taskHTML)
+const newTaskNameInput = document.querySelector("#TaskName");
+const newTaskDescriptionInput = document.querySelector("#newTaskDescriptionInput");
+const newAssignedInput = document.querySelector("#Assigned");
+const newDueDateInput = document.querySelector("#newDueDateInput");
 
 const validFormFieldInput = (e) => {
 	e.preventDefault();
-	const newTaskNameInput = document.querySelector("#TaskName");
 	const name = newTaskNameInput.value;
 	console.log("Task name:  " + name);
 
-	const newTaskDescriptionInput = document.querySelector("#newTaskDescriptionInput");
 	const description = newTaskDescriptionInput.value;
 	console.log("Task Description:  " + description);
 
-	const newAssignedInput = document.querySelector("#Assigned");
 	const assigned = newAssignedInput.value;
 	console.log("Assigned To:  " + assigned);
 
-	const newDueDateInput = document.querySelector("#newDueDateInput");
 	const dueDate = newDueDateInput.value;
 	console.log("Due Dute:  " + dueDate);
 
 	tasks.addTask(name, description, assigned, dueDate);
-	tasks.render()
+	tasks.render();
+	console.log(tasks)
 
-	document.getElementById('myForm').reset()
-
+	document.getElementById("myForm").reset();
 };
 
-if (newTaskDescriptionInput.value === "") {
-    document.getElementById("error").classList.add("fade");
-} else if (newTaskDescriptionInput.value === newTaskDescriptionInput.value)  {
-    document.getElementById("error").classList.add("show");
-}
+// Event listener for alert notification. Defines conditions for alert to appear
 
-const markAsDoneButton = document.querySelector('tasksList');
-markAsDoneButton.addEventListener('click', (event => {
+document.getElementById("btn").addEventListener("click", function() {
+	const name = newTaskNameInput.value;
 
-}))
+	const description = newTaskDescriptionInput.value;
+
+	const assigned = newAssignedInput.value;
+
+	const dueDate = newDueDateInput.value;
+
+	if (name === "") {
+		document.getElementById("error").innerHTML = `<div id="error" class="alert alert-primary" role="alert">
+	Please input name field!
+  </div>`;
+	} else if (description === "") {
+		document.getElementById("error").innerHTML = `<div id="error" class="alert alert-primary" role="alert">
+	Please input description field!
+  </div>`;
+	} else if (assigned === "") {
+		document.getElementById("error").innerHTML = `<div id="error" class="alert alert-primary" role="alert">
+	Please input assignedTo field!
+  </div>`;
+	} else if (dueDate === "") {
+		document.getElementById("error").innerHTML = `<div id="error" class="alert alert-primary" role="alert">
+Please input the Due Date field!
+</div>`;
+	} else {
+		document.getElementById("error").innerHTML = `<div id="error" class="alert alert-primary" role="alert">
+	Your task card been added!
+  </div>`;
+	}
+});
+
+// Event listerner for mark as done button. 
+
+const markAsDoneButton = document.querySelector('#tasksList');
+
+
+markAsDoneButton.addEventListener('click', (event) => {
+	console.log('Got it')
+	if (event.target.classList.contains('done-button')) {
+
+		let parentTask = event.target.parentNode.parentNode.parentNode.parentNode
+		console.log(parentTask)
+
+	}
+});
+
+
+
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,24 +1,24 @@
-const createTaskHtml = (name, description, assignedTo, dueDate, status) => {
-	return `        <li class="list-group-item ">
-	<div class="card" style="width: 18rem;">
+const createTaskHtml = (name, description, assignedTo, dueDate, status, id) => {
+	return `        <li class="list-group-item" id="data-task-${id}">
+	  <div class="card" style="width: 18rem;">
 		<div class="card-body">
-			<div class="alignment">
-		  <h5 class="card-title" id="title">Task Name: ${name}</h5>
-		  <button type="button" class="done-button btn btn-success">Mark as done</button>
-		</div>
+		  <div class="alignment">
+		    <h5 class="card-title" id="title">Task Name: ${name}</h5>
+		    <button type="button" class="done-button btn btn-success">Mark as done</button>
+		  </div>
 		  <h6 class="card-subtitle mb-2 text-muted">Task Description: ${description}</h6>
 		  <p class="card-text">Assigned Date: ${assignedTo}</p>
 		  <p class="card-text">Due Date: ${dueDate}</p>
 		  <div class="alignment">
-		  <p class="card-text">Status: ${status}</p>
-		  <div class="move">
-		  <a href="#" class="btn btn-primary">Delete</a>
-		</div>
-		</div>
+		    <p class="card-text">Status: ${status}</p>
+		    <div class="move">
+		      <a href="#" class="btn btn-primary">Delete</a>
+		    </div>
+		  </div>
 		</div>
 	  </div>
-</li>`
-}
+</li>`;
+};
 
 class TaskManager {
 	constructor(tasks, currentId) {
@@ -26,7 +26,7 @@ class TaskManager {
 		this._currentId = 0;
 	}
 
-	addTask(name, description, assignedTo, dueDate, status="TODO") {
+	addTask(name, description, assignedTo, dueDate, status = "TODO") {
 		this._currentId++;
 		const newTask = {
 			id: this._currentId,
@@ -35,29 +35,38 @@ class TaskManager {
 			assignedTo,
 			dueDate,
 			status
-		}
+		};
 		this._tasks.push(newTask);
 	}
 
 	render() {
-
 		const taskHtmlList = [];
 
 		// For of loop?
 		for (let task of this._tasks) {
-			console.log(task)
-			const taskHtml = createTaskHtml(task.name, task.description, task.assignedTo, task.dueDate, task.status)
+			const taskHtml = createTaskHtml(task.name, task.description, task.assignedTo, task.dueDate, task.status, task.id);
 			taskHtmlList.push(taskHtml);
-			const tasksHtml = taskHtmlList.join(''); // join("/n")
-			document.getElementById('taskCard').innerHTML = tasksHtml
+			const tasksHtml = taskHtmlList.join(""); // join("/n")
+			document.getElementById("taskCard").innerHTML = tasksHtml;
 		}
-		
+
 		// let currentTask = this._tasks;
 		// let date =  new Date(dueDate);
 		// let formattedDate = date.toString();
 		// let taskHtml = currentTask.createTaskHtml(formattedDate);
 		// taskHtmlList.push(taskHtml);
-
 	}
-}
 
+	getTaskById(taskId) {
+
+		let foundTask;
+
+		for (let task of this._tasks) {
+			if (task.id == taskId) {
+				task = foundTask;				
+			}
+		}
+		return foundTask
+	}
+
+}
